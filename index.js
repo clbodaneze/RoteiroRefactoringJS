2dc5d3b0bd2e0f2ec3b85973c94bef635696a576
CLBodaneze <clbodaneze@minha.fag.edu.br>
2026-06-16 08:28:46 -0300

    Commit 4 - Separando Apresentação dos Cálculos

--- index.js
+++ index.js
@@ -41,22 +41,33 @@ function gerarFaturaStr (fatura, pecas) {
           minimumFractionDigits: 2 }).format(valor / 100);
     }
 
-    let totalFatura = 0;
-    let creditosTotal = 0; 
-    let faturaStr = `Fatura ${fatura.cliente}\n`;
-  
-    for (let apre of fatura.apresentacoes) {
-      let total = calcularTotalApresentacao(apre);
-  
-      creditosTotal += calcularCredito(apre);
-      faturaStr += `  ${getPeca(apre).nome}: ${formatarMoeda(total)} (${apre.audiencia} assentos)\n`;
-      totalFatura += total;
+    function calcularTotalFatura() {
+      let total = 0;
+      for (let apre of fatura.apresentacoes) {
+        total += calcularTotalApresentacao(apre);
+      }
+      return total;
     }
 
-    faturaStr += `Valor total: ${formatarMoeda(totalFatura)}\n`;
-    faturaStr += `Créditos acumulados: ${creditosTotal} \n`;
+    function calcularTotalCreditos() {
+      let total = 0;
+      for (let apre of fatura.apresentacoes) {
+        total += calcularCredito(apre);
+      }
+      return total;
+    }
+
+
+    let faturaStr = `Fatura ${fatura.cliente}\n`;
+    
+    for (let apre of fatura.apresentacoes) {
+        faturaStr += `  ${getPeca(apre).nome}: ${formatarMoeda(calcularTotalApresentacao(apre))} (${apre.audiencia} assentos)\n`;
+    }
+    
+    faturaStr += `Valor total: ${formatarMoeda(calcularTotalFatura())}\n`;
+    faturaStr += `Créditos acumulados: ${calcularTotalCreditos()} \n`;
     return faturaStr;
-  }
+}
 
 const faturas = JSON.parse(readFileSync('./faturas.json'));
 const pecas = JSON.parse(readFileSync('./pecas.json'));
